37bfc493d5e54c5e49c54de61e4da9dad9d30e76
Yannlfc008 <113675276+Yannlfc008@users.noreply.github.com>
2022-10-07 11:02:26 +0200

    Create __init__.py

---
+++ src/morsa/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parents[2] / 'config'
+DORK_FILE_PATH = PROJECT_ROOT / 'YML_Dorks.yml'
+EXCEL_FILE = PROJECT_ROOT / 'Activos y observatorio de amenazas.xlsx'
+NEW_EXCEL_FILE = PROJECT_ROOT / 'Results.xlsx'
