59bbec1fec7f50989e9a6e0573b39693eae7b5a4
Yann <yleficher@sum.es>
2022-10-07 12:02:16 +0200

    New dorks

--- src/morsa/__init__.py
+++ src/morsa/__init__.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parents[2] / 'config'
 DORK_FILE_PATH = PROJECT_ROOT / 'YML_Dorks.yml'
-EXCEL_FILE = PROJECT_ROOT / 'Activos y observatorio de amenazas.xlsx'
+EXCEL_FILE = PROJECT_ROOT / 'Activos_y_amenazas.xlsx'
 NEW_EXCEL_FILE = PROJECT_ROOT / 'Results.xlsx'
