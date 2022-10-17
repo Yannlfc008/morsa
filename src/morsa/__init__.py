626a80a18fb5e172433ead273c1dab236929ad6c
Yannlfc008 <113675276+Yannlfc008@users.noreply.github.com>
2022-10-17 14:02:49 +0200

    Delete __init__.py

--- src/morsa/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-PROJECT_ROOT = Path(__file__).parents[2] / 'config'
-DORK_FILE_PATH = PROJECT_ROOT / 'YML_Dorks.yml'
-EXCEL_FILE = PROJECT_ROOT / 'Activos_y_amenazas.xlsx'
-NEW_EXCEL_FILE = PROJECT_ROOT / 'Results.xlsx'
